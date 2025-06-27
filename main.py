da7a1883ea0680241cdecba80715601893ec7f03
ricardoadamo <ricaadamo@gmail.com>
2025-06-27 10:51:35 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 from instaloader import Instaloader, Profile
-import pandas as pd
-import requests
-from bs4 import BeautifulSoup
 
 print("BOT_INFLUMETRICS funcionando!")
 
 L = Instaloader()
 print("Instaloader carregado!")
+
+username = 'influcall.bot'
+password = 'InflumetricsTest!'
+L.login(username, password)
+print("Login feito!")
+
+profile = Profile.from_username(L.context, 'instagram')
+
+followers = profile.get_followers()
+
+lista = []
+for seguidor in followers:
+    lista.append(seguidor.username)
+
+print("Seguidores capturados:", lista)
