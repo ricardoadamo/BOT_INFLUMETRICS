80b2e8acee0e23758f888c6ba29063ba95fd50da
ricardoadamo <ricaadamo@gmail.com>
2025-06-27 10:31:02 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+from instaloader import Instaloader, Profile
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+
+print("BOT_INFLUMETRICS funcionando!")
+
+L = Instaloader()
+print("Instaloader carregado!")
